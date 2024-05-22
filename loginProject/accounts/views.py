23c7f97749fdcccda877387d6f685172bdbfd40f
Tiago Couto <131413193+tiagolvjy@users.noreply.github.com>
2024-05-22 01:41:06 -0300

    Update views.py

--- loginProject/accounts/views.py
+++ loginProject/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import UserProfile
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = UserProfile.objects.filter(username=username, password=password).first()
+        if user:
+            # Usuário autenticado com sucesso
+            messages.success(request, f'Bem-vindo, {username}!')
+            return redirect('home')  # Redirecionar para a página inicial após o login
+        else:
+            # Credenciais inválidas
+            messages.error(request, 'Credenciais inválidas. Tente novamente.')
+
+    return render(request, 'accounts/login.html')
