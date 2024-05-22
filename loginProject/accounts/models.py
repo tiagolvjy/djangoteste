382d3e2c43a87caf6c3a6509491de14ce85f303a
Tiago Couto <131413193+tiagolvjy@users.noreply.github.com>
2024-05-22 01:40:25 -0300

    Update models.py

--- loginProject/accounts/models.py
+++ loginProject/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
